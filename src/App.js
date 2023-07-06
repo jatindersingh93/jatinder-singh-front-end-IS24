1beff8e751602b53a9cd12a66e56ada9df6a3ce6
jatinder <jatindersingh93@gmail.com>
2023-07-06 17:22:36 -0500

    add product list components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
-function App() {
-  return 'jatinder singh';
-}
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ProductsList from './components/ProductsList.component'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/products"} className="navbar-brand">
+            Products List
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/products"} className="nav-link">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<ProductsList/>} />
+            <Route path="/products" element={<ProductsList/>} />
+          </Routes>
+        </div>        
+      </Router>
+    );
+  }
+}
 export default App;
---
+++ src/components/ProductsList.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class ProductsList extends Component {
+    render() {   
+        return (
+          "jatinder singh"
+        );
+      }
+}
+
+export default ProductsList;
